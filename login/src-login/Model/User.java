d60e074556cb79c68da71971d1bb7c3c6d996b73
Mohsin545-ai <mohsinahmed20017@gmail.com>
2022-04-05 11:13:24 +0530

    added login

---
+++ login/src-login/Model/User.java
@@ -0,0 +1,24 @@
+package Model;
+
+public class User {
+    private String name;
+    private String password;
+
+    public User() {
+        // empty constructor
+    }
+
+    public User(String firstname, String lastname) {
+        this.name = firstname;
+        this.password = lastname;
+    }
+
+    // getters
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
